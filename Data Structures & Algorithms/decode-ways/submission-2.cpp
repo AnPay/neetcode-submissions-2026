d91ccda922a9398e78aa6d2d4d7e5c2f08a616cd
AnPay <42369232+AnPay@users.noreply.github.com>
2026-03-31 14:51:08 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if(n==0 || s[0]=='0')
+            return 0;
+        vector<int>dp(n+1,0);
+        dp[0]=1;  //empty string
+        dp[1]=1;  //length 1
+        for(int i =2;i<=n;i++)
+        {
+            //singledigit
+            if(s[i-1]!='0')
+                dp[i]+=dp[i-1];
+            //2 digits
+            int num = (s[i-2] - '0') * 10 + (s[i-1] - '0');
+            if(num>=10 && num<=26)
+            {
+                dp[i]+=dp[i-2];
+            }
+        } 
+        return dp[n];  
+    }
+};
+/*
+Pattern: String 1D DP
+Choice: Pick 1 digit or 2 digit
+State: dp[i] = #of ways to decode for string 0 to i-1
+Transition:
+if s[i]=="0"
+    dp[i]=0;
+take 1 digit: dp[i]+=dp[i-1]
+take 2 digit: dp[i]+=dp[i-2]
+
+Base case: dp[0]=1, dp[1]=1
+
+first of all i need a map/hash of number to character
+generate all possible sets of numbers from given string
+return count of sets generated
+
+now challenge is how to generate different arrangements of numbers
+if its 123, i can take 1 or leave 1
+so it will be 1, 23 or 12,3
+i can take 2 or leave 2
+so it will be
+1,2,3
+
+*/
