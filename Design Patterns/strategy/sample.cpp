86daa034087afdae3b1580641db904190d65a419
AnPay <42369232+AnPay@users.noreply.github.com>
2026-04-05 07:53:05 +0530

    Update sample.cpp

--- Design Patterns/strategy/sample.cpp
+++ Design Patterns/strategy/sample.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <memory>
 using namespace std;
 
 // Strategy Interface
 class PaymentStrategy {
 public:
     virtual string pay() = 0;
+virtual ~PaymentStrategy() {}  //Prevents undefined behavior when deleting via base pointer
 };
 
 // Concrete Strategies
@@ -33,6 +35,7 @@ public:
 };
 
 // Context
+//The context delegates the payment behavior to the strategy. This allows runtime switching and avoids if-else chains, making the system extensible
 class PaymentContext {
     PaymentStrategy* strategy;
 
@@ -43,11 +46,16 @@ public:
         // TODO
         return strategy->pay();
     }
+
+    void setStrategy(PaymentStrategy* s){
+        strategy=s;
+    }
 };
 
 // Example usage
 int main() {
     PaymentStrategy* s1 = new CreditCardPayment();
+    //unique_ptr<PaymentStrategy> s1 = make_unique<CreditCardPayment>(); //In production I’d prefer smart pointers to avoid memory leaks
     PaymentContext context1(s1);
     cout << context1.executePayment() << endl;
 
@@ -58,4 +66,10 @@ int main() {
     PaymentStrategy* s3 = new PayPalPayment();
     PaymentContext context3(s3);
     cout << context3.executePayment() << endl;
+
+    context1.setStrategy(s2);
+    cout << context1.executePayment() << endl;
+    delete s1;
+    delete s2;
+    delete s3;
 }
