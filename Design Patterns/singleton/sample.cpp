2fe16e128d671c88fa69ab3bcb727addfdf96a7b
AnPay <42369232+AnPay@users.noreply.github.com>
2026-04-05 08:26:05 +0530

    delete assignment and copy ctor

---
+++ Design Patterns/singleton/sample.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Logger {
+private:
+    // TODO:
+    // static instance pointer
+    static Logger* instance; //static very important
+    // private constructor
+    // Logger(){   
+    //   instance = new Logger();  ----> this is WRONG, it will initiate a recursive loop of creaating objects
+    // }
+    // Private constructor
+    Logger() {}   //“I’m making constructor private to prevent direct instantiation, and using a static instance to ensure only one object exists.”
+
+public:
+    // TODO:
+    // delete copy constructor & assignment operator
+    //“To prevent creation of multiple instances through copying, which would violate the Singleton guarantee.”
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    static Logger* getInstance() {
+        // TODO
+        if(instance==nullptr)
+          instance = new Logger();
+        
+        return instance;
+    }
+
+    void log(string message) {
+        cout << "Log: " << message << endl;
+    }
+};
+// Static member initialization
+Logger* Logger::instance = nullptr;
+
+int main() {
+    Logger* logger1 = Logger::getInstance();
+    Logger* logger2 = Logger::getInstance();
+
+    logger1->log("First message");
+    logger2->log("Second message");
+
+    if (logger1 == logger2) {
+        cout << "Same instance" << endl;
+    } else {
+        cout << "Different instances" << endl;
+    }
+}
