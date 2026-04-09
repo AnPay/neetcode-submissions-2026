56f92d542b070f568ea77c227f2bc809b36e255b
AnPay <42369232+AnPay@users.noreply.github.com>
2026-04-09 22:32:46 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int i;
+        int goal=nums.size()-1;
+        for(i=goal-1;i>=0;i--)
+        {
+            if(i+nums[i]>=goal)
+                goal=i;
+        } 
+        return goal==0;   
+    }
+};
